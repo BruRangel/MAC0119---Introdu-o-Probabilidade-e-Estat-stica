aee8796c9e9d4084190eca1d3908c25bb79da04e
Bruno Rangel <113265945+BruRangel@users.noreply.github.com>
2025-08-11 20:04:57 -0300

    Update lista_01.py

--- lista_01.py
+++ lista_01.py
@@ -1,7 +1,8 @@
 import math
 import statistics
 import numpy as np
-import scipy.stats
+import pandas as pd
+import scipy.stats  
 
 # Exercício 1
 X = [5.8, 1.4, 5.6, 0.0, 5.6, 1.3, 3.5, 6.6, 0.0, 6.3, 0.0, 6.9, 3.4, 4.1, 5.6, 6.7, 0.0, 3.4, 7.2, 7.8,
@@ -12,24 +13,29 @@ X = [5.8, 1.4, 5.6, 0.0, 5.6, 1.3, 3.5, 6.6, 0.0, 6.3, 0.0, 6.9, 3.4, 4.1, 5.6, 
 
 X.sort()
 print(f"X sorted: {X}")
-
-X_sum = sum(X)
-X_len = len(X)
-print(f"X_sum: {X_sum}")
-print(f"X_len: {X_len}")
-X_my_mean = X_sum/X_len
+N = len(X)
 
 # a)
+# Média aritmética simples
 X_mean = statistics.mean(X)
+# Mediana
 X_median = statistics.median(X)
+# Moda
 X_mode = statistics.mode(X)
+# Amplitude
 X_amplitude = abs(max(X) - min(X))
-X_quantiles = statistics.quantiles(X)
+# Quartis
+X_quantiles = statistics.quantiles(X) # O cálculo foi verificado observando os valores ordenados
+# Valor mínimo
 X_min_value = min(X)
+# Valor máximo
 X_max_value = max(X)
+# Cálculo do desvio médio:
 X_dm = np.mean([abs(x - X_mean) for x in X])
+# Cálculo do desvio padrão:
 X_dp = math.sqrt(np.mean([(x - X_mean)**2 for x in X]))
 
+# Exibindo Resultados:
 print(f"X_mean: {X_mean}")
 print(f"X_median: {X_median}")
 print(f"X_mode: {X_mode}")
@@ -39,30 +45,56 @@ print(f"X_min_value: {X_min_value}")
 print(f"X_max_value: {X_max_value}")
 print(f"X_desvio_medio: {X_dm}")
 print(f"X_desvio_padrao: {X_dp}")
-print(f"X_my_mean: {X_my_mean}")
 
 # b)
 qtd_entre_2dp = 0
-
+# Cálculo de quantos elementos estão no intervalo média +- 2DP
 for x in X:
     if (x >= X_mean - 2*X_dp) and (x <= X_mean + 2*X_dp):
         qtd_entre_2dp += 1
 
 print(f"Quantidade de elementos entre Média - 2DP e Média + 2DP: {qtd_entre_2dp}")
+
 media_menos_2dp = X_mean - X_dp * 2
 media_mais_2dp = X_mean + X_dp * 2
 print(f"Media menos 2dp: {media_menos_2dp}")
 print(f"Media mais 2dp: {media_mais_2dp}")
 
+# Criando as classes conforme especificadas no exercício
 X_0_3 = np.array([x for x in X if x>= 0 and x < 3])
 X_3_5 = np.array([x for x in X if x >= 3 and x < 5])
 X_5_7 = np.array([x for x in X if x >= 5 and x < 7])
 X_7_10 = np.array([x for x in X if x>= 7 and x < 10])
 
-print(len(X_0_3))
-print(len(X_3_5))
-print(len(X_5_7))
-print(len(X_7_10))
+classes_arrays = [X_0_3, X_3_5, X_5_7, X_7_10]
+class_labels = ["0 ⊢ 3", "3 ⊢ 5", "5 ⊢ 7", "7 ⊢ 10"]
+pontos_medios = [1.5, 4.0, 6.0, 8.5]
 
-X_K_mean = (1.5 * len(X_0_3) + 4 * len(X_3_5) + 6 * len(X_5_7) + 8.5 * len(X_7_10))/81
+# Amplitude de cada classe
+print(f"len(X_0_3): {len(X_0_3)}")
+print(f"len(X_3_5): {len(X_3_5)}")
+print(f"len(X_5_7): {len(X_5_7)}")
+print(f"len(X_7_10): {len(X_7_10)}")
+
+# Média aritmética dos dados agrupados
+X_K_mean = (1.5 * len(X_0_3) + 4 * len(X_3_5) + 6 * len(X_5_7) + 8.5 * len(X_7_10))/len(X)
 print(f"X_K_mean: {X_K_mean}")
+
+# É observável que a classe mediana é 5 -> 7
+# É observável que as classes modais são 0 -> 3 e 5 -> 7
+
+# Cálculo do Desvio Médio dos dados agrupados
+X_K_dm = (abs(1.5-X_K_mean)*len(X_0_3) +
+          abs(4-X_K_mean)*len(X_3_5) +
+          abs(6-X_K_mean)*len(X_5_7) +
+          abs(8.5-X_K_mean)*len(X_7_10))/N
+
+print(f"X_K_dm: {X_K_dm}")
+
+# Cálculo do Desvio Padrão dos dados agrupados
+X_K_dp = math.sqrt((((1.5-X_K_mean)**2)*len(X_0_3) +
+          ((4-X_K_mean)**2)*len(X_3_5) +
+          ((6-X_K_mean)**2)*len(X_5_7) +
+          ((8.5-X_K_mean)**2)*len(X_7_10))/N)
+
+print(f"X_K_dp: {X_K_dp}")
